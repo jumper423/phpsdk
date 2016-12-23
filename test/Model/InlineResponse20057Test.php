0e5783e763d03ffeb13bb06bc1c27d91f523f348
Dmitry Ivaneychik <ivaneychik@zingaya.com>
2016-12-23 16:20:50 +0300

    and now package names

--- test/Model/InlineResponse20057Test.php
+++ test/Model/InlineResponse20057Test.php
@@ -5,7 +5,7 @@
  * PHP version 5
  *
  * @category Class
- * @package  Swagger\Client
+ * @package  Voximplant
  * @author   http://github.com/swagger-api/swagger-codegen
  * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache Licene v2
  * @link     https://github.com/swagger-api/swagger-codegen
@@ -39,7 +39,7 @@
  * Please update the test case below to test the model.
  */
 
-namespace Swagger\Client;
+namespace Voximplant;
 
 /**
  * InlineResponse20057Test Class Doc Comment
@@ -47,7 +47,7 @@ namespace Swagger\Client;
  * @category    Class */
 // * @description InlineResponse20057
 /**
- * @package     Swagger\Client
+ * @package     Voximplant
  * @author      http://github.com/swagger-api/swagger-codegen
  * @license     http://www.apache.org/licenses/LICENSE-2.0 Apache Licene v2
  * @link        https://github.com/swagger-api/swagger-codegen
